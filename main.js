4d4b685104e78b0e701379a4405faeadbfa91a71
Kenneth <yancheng1112@sjtu.edu.cn>
2019-12-23 18:00:29 +0800

    update css and numbergrids

--- main.js
+++ main.js
@@ -2,8 +2,8 @@
 const canvas = document.querySelector('canvas');
 const ctx = canvas.getContext('2d');
 
-const width = canvas.width = window.innerWidth - 20;
-const height = canvas.height = window.innerHeight - 20;
+const width = canvas.width;
+const height = canvas.height;
 
 class FilletRectangle
 {
@@ -64,7 +64,7 @@ class NumberGrids
     {
         this.numbers = [[2, 0, 0, 0], [0, 4, 0, 0], [0, 0, 8, 0], [0, 0, 0, 0]];
         this.num2color = new Map([[2, "rgb(255, 222, 173)"], [4, "rgb(255, 218, 185)"], [8, "rgb(238, 220, 130)"], [16, "rgb(205, 198, 115)"], [32, "rgb(255, 193, 37)"], [64, "rgb(205, 155, 29)"], [128, "rgb(139, 117, 0)"], [256, "rgb(210, 105, 30)"], [512, "rgb(160, 82, 45)"], [1024, "rgb(139, 69, 19)"], [2048, "rgb(165, 42, 42)"]]);
-        this.text = document.getElementsByClassName("numbers");
+        this.text = document.getElementById("numbers");
     }
 
     draw()
@@ -80,11 +80,11 @@ class NumberGrids
                         color = this.num2color.get(2048);
                     let rec = new FilletRectangle(width / 2 - height / 2 + j * (gap + grid_size) + gap, i * (gap + grid_size) + gap, grid_size, grid_size, 5, color);
                     rec.draw();
-                    let newContent = document.createElement("h1"); 
-                    newContent.setAttribute("position", "absolute");
-                    newContent.setAttribute("left", String(width / 2 - height / 2 + j * (gap + grid_size) + gap));
-                    newContent.setAttribute("top", String(i * (gap + grid_size) + gap));
-                    newContent.textContent(String(this.numbers[i][j]));
+                    let newContainer = document.createElement("div");
+                    let newContent = document.createElement("span"); 
+                    newContent.setAttribute("padding-left", String(width / 2 - height / 2 + j * (gap + grid_size) + gap) + "px");
+                    newContent.setAttribute("padding-top", String(i * (gap + grid_size) + gap) + "px");
+                    newContent.textContent = String(this.numbers[i][j]);
                     this.text.appendChild(newContent);
                 }
     }
